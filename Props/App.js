47fe004d258969ef96cb85a898926ee3b03b2b55
leenanadkar <leena.nadkar12@gmail.com>
2025-01-21 15:37:26 +0530

    Create App.js

---
+++ Props/App.js
@@ -0,0 +1,14 @@
+// App.js
+import Greeting from "./Greeting";
+
+function App() {
+  return (
+    <div>
+      <h1>Using Props</h1>
+      <Greeting name="Leena" />
+      <Greeting name="John" />
+    </div>
+  );
+}
+
+export default App;
