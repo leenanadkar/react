a3b0aaf7978551dfd92c2c8bf6238142c0b0f4af
leenanadkar <leena.nadkar12@gmail.com>
2025-01-21 15:35:35 +0530

    Update App.js

--- Components/App.js
+++ Components/App.js
@@ -1,1 +1,16 @@
+// App.js
+import FunctionalComponent from "./FunctionalComponent";
+import ClassComponent from "./ClassComponent";
+
+function App() {
+  return (
+    <div>
+      <h1>React Components</h1>
+      <FunctionalComponent />
+      <ClassComponent />
+    </div>
+  );
+}
+
+export default App;
 
