0a6694d245ca0955623fa8a2971353b8f27a8644
leenanadkar <leena.nadkar12@gmail.com>
2025-01-22 12:20:55 +0530

    Create App.js

---
+++ SimpleForm/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import SimpleForm from "./components/SimpleForm";
+
+function App() {
+
+  return(
+    <div>
+    <SimpleForm/>
+    </div>
+    );
+}
+
+
+export default App;
